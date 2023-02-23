513fce4f105b1dff71ee5cbedd96b58238ac370b
Tomasz Michalak <8047609+tomaszmichalak@users.noreply.github.com>
2023-02-24 00:26:06 +0100

    Message order scenario: message redelivery with delay

--- scenarios/src/test/java/pl/tmlconsulting/pulsar/scenarios/MessagesOrderScenariosTest.java
+++ scenarios/src/test/java/pl/tmlconsulting/pulsar/scenarios/MessagesOrderScenariosTest.java
@@ -3,6 +3,7 @@ package pl.tmlconsulting.pulsar.scenarios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.concurrent.TimeUnit;
 import org.apache.pulsar.client.api.Consumer;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.Producer;
@@ -20,38 +21,69 @@ public class MessagesOrderScenariosTest {
   private final PulsarClient client = createClient();
 
   @Test
-  void expectOrderedMessagesWhenAllAcknowledged() throws PulsarClientException {
+  void expectMessagesInOrderWhenAck() throws PulsarClientException {
     String topicName = TOPIC_NAME + System.currentTimeMillis();
-    Producer<String> producer = client.newProducer(Schema.STRING)
-        .topic(topicName)
-        .create();
+    Producer<String> producer = getProducer(topicName);
 
     producer.send("A");
     producer.send("B");
     producer.close();
 
-    Consumer<String> testConsumer = client.newConsumer(Schema.STRING)
-        .topic(topicName)
+    Consumer<String> consumer = client.newConsumer(Schema.STRING).topic(topicName)
+        .subscriptionName("unit-test")
+        .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+        .subscriptionType(SubscriptionType.Exclusive).subscribe();
+
+    Message<String> m1 = consumer.receive();
+    consumer.acknowledge(m1);
+    Message<String> m2 = consumer.receive();
+    consumer.acknowledge(m1);
+    consumer.close();
+
+    assertEquals("A", m1.getValue());
+    assertEquals("B", m2.getValue());
+  }
+
+  @Test
+  void expectMessageRedeliveryWithDelayWhenNegativeAck() throws PulsarClientException {
+    String topicName = TOPIC_NAME + System.currentTimeMillis();
+    Producer<String> producer = getProducer(topicName);
+
+    producer.send("A");
+    producer.send("B");
+
+    Consumer<String> consumer = client.newConsumer(Schema.STRING).topic(topicName)
         .subscriptionName("unit-test")
         .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
         .subscriptionType(SubscriptionType.Exclusive)
-        .subscribe();
+        .negativeAckRedeliveryDelay(1, TimeUnit.SECONDS).subscribe();
 
-    Message<String> messageAttempt1 = testConsumer.receive();
-    testConsumer.acknowledge(messageAttempt1);
-    Message<String> messageAttempt2 = testConsumer.receive();
-    testConsumer.acknowledge(messageAttempt1);
-    testConsumer.close();
+    Message<String> m1 = consumer.receive();
+    consumer.negativeAcknowledge(m1);
+    Message<String> m2 = consumer.receive();
+    consumer.acknowledge(m2);
+    producer.send("C");
+    Message<String> m3 = consumer.receive();
+    consumer.acknowledge(m3);
+    Message<String> m4 = consumer.receive();
+    consumer.acknowledge(m4);
 
-    assertEquals("A", messageAttempt1.getValue());
-    assertEquals("B", messageAttempt2.getValue());
+    assertEquals("A", m1.getValue());
+    assertEquals("B", m2.getValue());
+    assertEquals("C", m3.getValue());
+    assertEquals("A", m4.getValue());
+
+    producer.close();
+    consumer.close();
+  }
+
+  private Producer<String> getProducer(String topicName) throws PulsarClientException {
+    return client.newProducer(Schema.STRING).topic(topicName).create();
   }
 
   private PulsarClient createClient() {
     try {
-      return PulsarClient.builder()
-          .serviceUrl("pulsar://localhost:6650")
-          .build();
+      return PulsarClient.builder().serviceUrl("pulsar://localhost:6650").build();
     } catch (PulsarClientException e) {
       throw new RuntimeException(e);
     }
